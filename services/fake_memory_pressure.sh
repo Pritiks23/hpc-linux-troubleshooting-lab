3da10a2c83fb540a47ee42ed6e8f44ab966cc155
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-06-15 11:54:16 -0700

    Update fake_memory_pressure.sh

--- services/fake_memory_pressure.sh
+++ services/fake_memory_pressure.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-python3 - <<'PY'
-import time
-chunks = []
-for _ in range(100):
-    chunks.append('x' * 10000000)
-print('Allocated memory chunks:', len(chunks))
-time.sleep(60)
-PY
+
